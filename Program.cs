83247e58310a1577496a206d57c59b7ad8b9587f
Robert <robertkremer.frl@gmail.com>
2019-05-22 09:45:50 +0200

    Added basic gui

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace La_Vita_e_Bella
 {
-    class Program
+    /* Main application class */
+    public class Program : Form
     {
-        static void Main(string[] args)
+        [STAThread]
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Mama mia");
-            Console.WriteLine("Papa pia");
-            Console.WriteLine("Baby got that diarrhoea");
-            Console.ReadLine();
+            /* Show gui */
+            new Program().ShowDialog();
+        }
+
+        private Program()
+        {
+            Text = "epic";
         }
     }
 }
