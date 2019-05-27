11ea2b6c30081f0d7772a7507461593a424ec2f5
Ed <215186@edu.rocfriesepoort.nl>
2019-05-27 12:37:56 +0200

    Added Login gui class

---
+++ gui/guis/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/* Étoine */
+namespace La_Vita_e_Bella.gui.guis
+{
+    public class Login : Gui
+    {
+        public Login()
+        {
+            Name = Text = "Hallo";
+            hi();
+        }
+
+        public void hi()
+        {
+            Button next = AddButton("->", Color.White, Width - 61, 0, 20, 20);
+            Label hallo = AddLabel("Hallo", Color.Red, 50, 10, 20, 20);
+            next.Click += volgende;
+        }
+
+        private void volgende(object butt, EventArgs args)
+        {
+            Hide();
+            Show();
+        }
+    }
+}
